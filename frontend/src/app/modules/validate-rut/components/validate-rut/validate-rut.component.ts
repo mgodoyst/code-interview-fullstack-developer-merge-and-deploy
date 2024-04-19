e37f9669d3349ffccfb0a6dd82d8500286377d86
Mauricio Godoy <mgodoy@stacktrace.com.ar>
2024-04-19 00:00:29 -0300

    Validaciones de formulario

--- frontend/src/app/modules/validate-rut/components/validate-rut/validate-rut.component.ts
+++ frontend/src/app/modules/validate-rut/components/validate-rut/validate-rut.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ValidateRutService } from '../../../../services/validate-rut.service';
 import { ValidateRut } from '../../../../models/validate-rut.model';
 import { ToastrService } from 'ngx-toastr';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-validate-rut',
@@ -9,16 +10,28 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ValidateRutComponent {
 
-  constructor(private validateRutService: ValidateRutService, private toastr: ToastrService) { }
+  constructor(private validateRutService: ValidateRutService,
+    private toastr: ToastrService,
+    private fb: FormBuilder) {
+    this.initForm()
+  }
 
   rut: string = ""
   validatorDigit: string = ""
+  form!: FormGroup;
+
+  initForm() {
+    this.form = this.fb.group({
+      rut: ["", [Validators.required, Validators.minLength(6), Validators.maxLength(8)]],
+      validatorDigit: ["", [Validators.required, Validators.maxLength(1)]]
+    })
+    this.subscribeToFieldChanges()
+  }
 
   validate() {
-    const validateRut: ValidateRut = {
-      rut: this.rut,
-      validatorDigit: this.validatorDigit,
-    }
+    if (this.form.invalid) return;
+
+    const validateRut: ValidateRut = this.form.value
 
     this.validateRutService.validate(validateRut)
       .subscribe(data => {
@@ -30,5 +43,21 @@ export class ValidateRutComponent {
     if (isError)
       this.toastr.success(message, '', { positionClass: 'toast-bottom-center' });
     else this.toastr.error(message, '', { positionClass: 'toast-bottom-center' })
+  }
+
+  get rutField() {
+    return this.form.get("rut")
+  }
+
+  get validatorDigitField() {
+    return this.form.get("validatorDigit")
+  }
+
+  subscribeToFieldChanges() {
+    this.rutField?.valueChanges.subscribe((value: string) =>
+      this.rutField?.patchValue(value.replace(/\D/g, '')))
+
+    this.validatorDigitField?.valueChanges.subscribe((value: string) =>
+      this.validatorDigitField?.patchValue(value.replace(/[^0-9kK]/g, '')));
   }
 }
